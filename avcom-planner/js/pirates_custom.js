14addee2ac0726aec50c9b575416d792ce0cc5ff
Kaspar-PotBS <94735202+Kaspar-PotBS@users.noreply.github.com>
2022-11-18 06:54:07 +0100

    Add files via upload

---
+++ avcom-planner/js/pirates_custom.js
@@ -0,0 +1,653 @@
+      $(document).ready(function () {
+
+
+         // Beschreibung bei Hover einblenden, lost-focus ausblenden
+          $(".Beschreibung.00 img").hover(function () {
+              $(".discription").hide("fast");
+              $(".discription.00").show(200);
+
+
+              }, function () {
+                $(".discription.00").hide("fast");
+            });
+
+            $(".Beschreibung.11 img").hover(function () {
+			  $(".discription").hide("fast");              
+			  $(".discription.11").show(200);
+
+            }, function () {
+                $(".discription.11").hide("fast");
+            });
+
+            $(".Beschreibung.12 img").hover(function () {
+	            $(".discription").hide("fast");
+                $(".discription.12").show(200);
+
+            }, function () {
+                $(".discription.12").hide("fast");
+            });
+
+            $(".Beschreibung.13 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.13").show(200);
+            }, function () {
+                $(".discription.13").hide("fast");
+            });
+
+            $(".Beschreibung.14 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.14").show(200);
+            }, function () {
+                $(".discription.14").hide("fast");
+            });
+
+            $(".Beschreibung.15 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.15").show(200);
+            }, function () {
+                $(".discription.15").hide("fast");
+            });
+			$(".Beschreibung.16 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.16").show(200);
+            }, function () {
+                $(".discription.16").hide("fast");
+            });
+			$(".Beschreibung.17 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.17").show(200);
+            }, function () {
+                $(".discription.17").hide("fast");
+            });
+			$(".Beschreibung.18 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.18").show(200);
+            }, function () {
+                $(".discription.18").hide("fast");
+            });
+
+            $(".Beschreibung.21 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.21").show(200);
+            }, function () {
+                $(".discription.21").hide("fast");
+            });
+
+            $(".Beschreibung.22 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.22").show(200);
+            }, function () {
+                $(".discription.22").hide("fast");
+            });
+
+            $(".Beschreibung.23 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.23").show(200);
+            }, function () {
+                $(".discription.23").hide("fast");
+            });
+
+            $(".Beschreibung.24 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.24").show(200);
+            }, function () {
+                $(".discription.24").hide("fast");
+            });
+
+            $(".Beschreibung.25 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.25").show(200);
+            }, function () {
+                $(".discription.25").hide("fast");
+            });
+			
+			$(".Beschreibung.26 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.26").show(200);
+            }, function () {
+                $(".discription.26").hide("fast");
+            });
+			
+			$(".Beschreibung.27 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.27").show(200);
+            }, function () {
+                $(".discription.27").hide("fast");
+            });
+			
+			$(".Beschreibung.28 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.28").show(200);
+            }, function () {
+                $(".discription.28").hide("fast");
+            });
+
+            $(".Beschreibung.31 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.31").show(200);
+            }, function () {
+                $(".discription.31").hide("fast");
+            });
+
+            $(".Beschreibung.32 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.32").show(200);
+            }, function () {
+                $(".discription.32").hide("fast");
+            });
+
+            $(".Beschreibung.33 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.33").show(200);
+            }, function () {
+                $(".discription.33").hide("fast");
+            });
+
+            $(".Beschreibung.34 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.34").show(200);
+            }, function () {
+                $(".discription.34").hide("fast");
+            });
+			
+			$(".Beschreibung.35 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.35").show(200);
+            }, function () {
+                $(".discription.35").hide("fast");
+            });
+
+            $(".Beschreibung.36 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.36").show(200);
+            }, function () {
+                $(".discription.36").hide("fast");
+            });
+			
+			$(".Beschreibung.37 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.37").show(200);
+            }, function () {
+                $(".discription.37").hide("fast");
+            });
+			
+			$(".Beschreibung.38 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.38").show(200);
+            }, function () {
+                $(".discription.38").hide("fast");
+            });
+
+            $(".Beschreibung.41 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.41").show(200);
+            }, function () {
+                $(".discription.41").hide("fast");
+            });
+
+            $(".Beschreibung.42 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.42").show(200);
+            }, function () {
+                $(".discription.42").hide("fast");
+            });
+
+            $(".Beschreibung.43 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.43").show(200);
+            }, function () {
+                $(".discription.43").hide("fast");
+            });
+
+            $(".Beschreibung.44 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.44").show(200);
+            }, function () {
+                $(".discription.44").hide("fast");
+            });
+
+            $(".Beschreibung.45 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.45").show(200);
+            }, function () {
+                $(".discription.45").hide("fast");
+            });
+			
+			$(".Beschreibung.46 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.46").show(200);
+            }, function () {
+                $(".discription.46").hide("fast");
+            });
+			
+			$(".Beschreibung.47 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.47").show(200);
+            }, function () {
+                $(".discription.47").hide("fast");
+            });
+			
+			$(".Beschreibung.48 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.48").show(200);
+            }, function () {
+                $(".discription.48").hide("fast");
+            });
+			
+			$(".Beschreibung.50 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.50").show(200);
+            }, function () {
+                $(".discription.50").hide("fast");
+            });
+			
+			$(".Beschreibung.51 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.51").show(200);
+            }, function () {
+                $(".discription.51").hide("fast");
+            });
+			
+			$(".Beschreibung.52 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.52").show(200);
+            }, function () {
+                $(".discription.52").hide("fast");
+            });
+			
+			$(".Beschreibung.53 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.53").show(200);
+            }, function () {
+                $(".discription.53").hide("fast");
+            });
+			
+			$(".Beschreibung.54 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.54").show(200);
+            }, function () {
+                $(".discription.54").hide("fast");
+            });
+			
+			$(".Beschreibung.55 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.55").show(200);
+            }, function () {
+                $(".discription.55").hide("fast");
+            });
+			
+			$(".Beschreibung.56 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.56").show(200);
+            }, function () {
+                $(".discription.56").hide("fast");
+            });
+			
+			$(".Beschreibung.57 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.57").show(200);
+            }, function () {
+                $(".discription.57").hide("fast");
+            });
+			
+			$(".Beschreibung.58 img").hover(function () {
+	            $(".discription").hide("fast");            
+                $(".discription.58").show(200);
+            }, function () {
+                $(".discription.58").hide("fast");
+            });
+            
+            $("#contentRight").click(function () {
+	            $(".discription").hide("fast");            
+            });
+            
+            $(".discription").click(function () {
+	            $(".discription").hide("fast");            
+            });
+            
+            
+      });
+
+
+
+        $(function () {
+            var select = $("#tab1");
+			var skill1value;
+			skillslider1 = 0;
+            var slider1 = $('<div id="slider1"></div>').insertAfter(select).slider({
+                value: 0,
+                min: 0,
+                max: 8,
+                range: "min",
+                slide: function (event, ui) {
+                    $("#skill-1").val(ui.value);
+					let skill1value = $("#skill-1").val();
+					
+					if (skill1value == 8) { skillslider1 = 13;
+					   
+					   }   else { 
+
+					if (skill1value == 7) { skillslider1 = 10;
+					   
+					   
+						}   else {
+
+					if (skill1value == 6) { skillslider1 = 8;
+					   
+					   
+						}   else { 
+						
+					if (skill1value == 5) { skillslider1 = 6;
+					   
+					   
+						}   else { 
+						
+					if (skill1value == 4) { skillslider1 = 4;
+					   
+					   
+						}   else { 
+						
+					if (skill1value == 3) { skillslider1 = 3;
+					   
+					   
+						}   else { 
+						
+					if (skill1value == 2) { skillslider1 = 2;
+					   
+					   
+						}   else { 
+
+					if (skill1value == 1) { skillslider1 = 1;
+					   
+					   
+						}   else { skillslider1 = 0;
+						
+}						
+}
+}
+}
+}
+}
+}
+}
+
+				
+					
+					
+					
+                    $("#UsedPoints").text(skillslider1 + skillslider2 + skillslider3 + skillslider4);
+					
+
+                    // update SkillCode
+                    var skillcode = $("#skill-1").val() + $("#skill-2").val() + $("#skill-3").val() + $("#skill-4").val();
+                    $("#SkillCode").val(skillcode);
+
+                    if ($("#UsedPoints").text() > 25) {
+                        // alert('hallo');
+                        $("#Points").css("background-color", "#a52a2a");
+
+                    } else { $("#Points").css("background-color", "#2e905f"); }
+
+
+
+
+                }
+            });
+        });
+		
+		
+
+
+        $(function () {
+            var select = $("#tab2");
+			var skill2value;
+			skillslider2 = 0;
+            var slider2 = $('<div id="slider2"></div>').insertAfter(select).slider({
+                value: 0,
+                min: 0,
+                max: 8,
+                range: "min",
+                slide: function (event, ui) {
+                    $("#skill-2").val(ui.value);
+					let skill2value = $("#skill-2").val();
+					
+					if (skill2value == 8) { skillslider2 = 13;
+					   
+					   }   else { 
+
+					if (skill2value == 7) { skillslider2 = 10;
+					   
+					   
+						}   else {
+
+					if (skill2value == 6) { skillslider2 = 8;
+					   
+					   
+						}   else { 
+						
+					if (skill2value == 5) { skillslider2 = 6;
+					   
+					   
+						}   else { 
+						
+					if (skill2value == 4) { skillslider2 = 4;
+					   
+					   
+						}   else { 
+						
+					if (skill2value == 3) { skillslider2 = 3;
+					   
+					   
+						}   else { 
+						
+					if (skill2value == 2) { skillslider2 = 2;
+					   
+					   
+						}   else { 
+
+					if (skill2value == 1) { skillslider2 = 1;
+					   
+					   
+						}   else { skillslider2 = 0;
+						
+}						
+}
+}
+}
+}
+}
+}
+}
+
+					
+					
+					
+					
+                    $("#UsedPoints").text(skillslider2 + skillslider1 + skillslider3 + skillslider4);
+
+                    // update SkillCode
+                    var skillcode = $("#skill-1").val() + $("#skill-2").val() + $("#skill-3").val() + $("#skill-4").val();
+                    $("#SkillCode").val(skillcode);
+
+                    if ($("#UsedPoints").text() > 25) {
+                        // alert('hallo');
+                        $("#Points").css("background-color", "#a52a2a");
+
+                    } else { $("#Points").css("background-color", "#2e905f"); }
+
+
+
+
+                }
+            });
+        });
+
+
+        $(function () {
+            var select = $("#tab3");
+			var skill3value;
+			skillslider3 = 0;
+            var slider3 = $('<div id="slider3"></div>').insertAfter(select).slider({
+                value: 0,
+                min: 0,
+                max: 8,
+                range: "min",
+                slide: function (event, ui) {
+                    $("#skill-3").val(ui.value);
+					let skill3value = $("#skill-3").val();
+					
+					if (skill3value == 8) { skillslider3 = 13;
+					   
+					   }   else { 
+
+					if (skill3value == 7) { skillslider3 = 10;
+					   
+					   
+						}   else {
+
+					if (skill3value == 6) { skillslider3 = 8;
+					   
+					   
+						}   else { 
+						
+					if (skill3value == 5) { skillslider3 = 6;
+					   
+					   
+						}   else { 
+						
+					if (skill3value == 4) { skillslider3 = 4;
+					   
+					   
+						}   else { 
+						
+					if (skill3value == 3) { skillslider3 = 3;
+					   
+					   
+						}   else { 
+						
+					if (skill3value == 2) { skillslider3 = 2;
+					  
+					   
+						}   else { 
+
+					if (skill3value == 1) { skillslider3 = 1;
+					   
+					   
+						}   else { skillslider3 = 0;
+						
+}						
+}
+}
+}
+}
+}
+}
+}
+
+					
+					
+					
+					
+					
+                    $("#UsedPoints").text(skillslider3 + skillslider1 + skillslider2 + skillslider4);
+
+                    // update SkillCode
+                    var skillcode = $("#skill-1").val() + $("#skill-2").val() + $("#skill-3").val() + $("#skill-4").val();
+                    $("#SkillCode").val(skillcode);
+
+                    if ($("#UsedPoints").text() > 25) {
+                        // alert('hallo');
+                        $("#Points").css("background-color", "#a52a2a");
+
+                    } else { $("#Points").css("background-color", "#2e905f"); }
+
+
+
+
+                }
+            });
+        });
+
+
+        $(function () {
+            var select = $("#tab4");
+			var skill4value;
+			skillslider4 = 0;
+            var slider4 = $('<div id="slider4"></div>').insertAfter(select).slider({
+                value: 0,
+                min: 0,
+                max: 8,
+                range: "min",
+                slide: function (event, ui) {
+                    $("#skill-4").val(ui.value);
+					let skill4value = $("#skill-4").val();
+					
+					if (skill4value == 8) { skillslider4 = 13;
+					   
+					   }   else { 
+
+					if (skill4value == 7) { skillslider4 = 10;
+					   
+					   
+						}   else {
+
+					if (skill4value == 6) { skillslider4 = 8;
+					   
+					   
+						}   else { 
+						
+					if (skill4value == 5) { skillslider4 = 6;
+					   
+					   
+						}   else { 
+						
+					if (skill4value == 4) { skillslider4 = 4;
+					   
+					   
+						}   else { 
+						
+					if (skill4value == 3) { skillslider4 = 3;
+					   
+					   
+						}   else { 
+						
+					if (skill4value == 2) { skillslider4 = 2;
+					   
+					   
+						}   else { 
+
+					if (skill4value == 1) { skillslider4 = 1;
+					   
+					   
+						}   else { skillslider4 = 0;
+						
+}						
+}
+}
+}
+}
+}
+}
+}
+
+
+
+                    $("#UsedPoints").text(skillslider4 + skillslider1 + skillslider2 + skillslider3);
+
+                    // update SkillCode
+                    var skillcode = $("#skill-1").val() + $("#skill-2").val() + $("#skill-3").val() + $("#skill-4").val();
+                    $("#SkillCode").val(skillcode);
+
+                    if ($("#UsedPoints").text() > 25) {
+                        // alert('hallo');
+                        $("#Points").css("background-color", "#a52a2a");
+
+                    } else { $("#Points").css("background-color", "#2e905f"); }
+
+
+
+
+                }
+            });
+        });
+
